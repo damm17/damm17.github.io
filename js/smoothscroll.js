e4fb3506f4acb6ded0d3bf6d94f5c88c7fbe6ec5
Damian Bertazza <damian.bertazza@gmail.com>
2015-03-11 20:10:54 -0300

    Actualizaciones
    Interacción con botones y demás.

---
+++ js/smoothscroll.js
@@ -0,0 +1,44 @@
+$(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
+
+e.preventDefault();
+
+var hash = this.hash;
+
+$('html, body').animate({
+	scrollTop: $(this.hash).offset().top -50
+	}, 700, function(){
+
+	window.location.hash = hash;
+	});
+
+});
+
+$("a.navbar-brand[href^='#']").on('click', function(e) {
+
+e.preventDefault();
+
+var hash = this.hash;
+
+$('html, body').animate({
+	scrollTop: $(this.hash).offset().top -50
+	}, 700, function(){
+
+	window.location.hash = hash;
+	});
+
+});
+
+$("#back-to-top a[href^='#']").on('click', function(e) {
+
+e.preventDefault();
+
+var hash = this.hash;
+
+$('html, body').animate({
+	scrollTop: $(this.hash).offset().top -50
+	}, 700, function(){
+
+	window.location.hash = hash;
+	});
+
+});
